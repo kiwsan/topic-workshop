5616aebd8c7e1ef45905b310b55017e6a59d845f
kiwsan <kiwsanthia@gmail.com>
2019-05-18 22:12:51 +0700

    remove appcontants

--- Utils/AppConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Topic.Utils
-{
-    public static class AppConstants
-    {
-        public const string ConnectionString = "Server=localhost;Database=docker;User Id=sa; Password=Str0ngPassword!;";
-    }
-}
