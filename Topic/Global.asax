3ea84534460bf44c08ed2b6faf0cb5c2f59ae631
kiwsan <kiwsanthia@gmail.com>
2019-05-24 14:03:23 +0700

    add templates

--- Topic/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Topic.MvcApplication" Language="C#" %>
--- UnitTests/Properties/AssemblyInfo.cs
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("08A379E0-D7DF-43B3-8AD5-B3AAB7E836B9")]
+[assembly: Guid("A390FD2D-65B0-44E2-8177-390517C852B6")]
 
 // Version information for an assembly consists of the following four values:
 //
